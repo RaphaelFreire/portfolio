5975c293f4ca6b7c40b2b0aa75a18e42510c4a59
Raphael Freire <raphaelmachadofreire@gmail.com>
2019-03-17 00:10:04 -0300

    ajustando manifest

--- js/sw.js
+++ js/sw.js
@@ -1,56 +1,25 @@
----
-layout: null
----
+var SLOW_TIME = 3000;
 
-const staticCacheName = 'raphael-freire-{{ site.time | date: "%Y-%m-%d-%H-%M" }}';
-
-const filesToCache = [
-  {% for page in site.pages_to_cache %}
-    '{{ page }}',
-  {% endfor %}
-  {% for post in site.posts limit: 6%}
-    '{{ post.url }}',
-  {% endfor %}
-  {% for asset in site.files_to_cache %}
-    '{{ asset }}',
-  {% endfor %}
-];
-
-// Cache on install
-this.addEventListener("install", event => {
-  this.skipWaiting();
-
-  event.waitUntil(
-    caches.open(staticCacheName)
-      .then(cache => {
-        return cache.addAll(filesToCache);
-    })
-  )
+this.addEventListener("install", function() {
+	console.log("Installed service worker");
 });
 
-// Clear cache on activate
-this.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(cacheName => (cacheName.startsWith('raphael-freire-')))
-          .filter(cacheName => (cacheName !== staticCacheName))
-          .map(cacheName => caches.delete(cacheName))
-      );
-    })
-  );
-});
+this.addEventListener("fetch", function(event) {
+	var url = event.request.url;
 
-// Serve from Cache
-this.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-      .catch(() => {
-        return caches.match('/offline/index.html');
-      })
-  )
+	if (url.indexOf("blocking") === -1) {
+		return;
+	}
+
+	var promise = Promise.race([
+		new Promise((resolve, reject) =>
+			setTimeout(
+				() => reject(new Response("Request killed!")),
+				SLOW_TIME
+			)
+		),
+		fetch(event.request)
+	]);
+
+	event.respondWith(promise);
 });
