6ba12a74ec5071adc30bc11d66e7687a77d43afb
Raphael Freire <raphaelmachadofreire@gmail.com>
2019-03-28 23:06:36 -0300

    tentando ajustar cache

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // app.js
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", function() {
-		navigator.serviceWorker.register("sw.js").then(
+		navigator.serviceWorker.register("/js/sw.js").then(
 			function(registration) {
 				// Registration was successful
 				console.log(
--- js/sw.js
+++ js/sw.js
@@ -1,6 +1,39 @@
 // sw.js
-const cacheName = "PWA-v1";
-const filesToCache = ["index.html"];
+const cacheName = "RaphaelPWA-v1";
+const filesToCache = [
+	"/index.html",
+	"/css/generic/base.css",
+	"/css/generic/reset.min.css",
+	"/css/components/footer.css",
+	"/css/components/formulario.css",
+	"/css/components/four.css",
+	"/css/components/primary-max-320.css",
+	"/css/components/primary-max-450.css",
+	"/css/components/primary-max-600.css",
+	"/css/components/primary-max-750.css",
+	"/css/components/primary.css",
+	"/css/components/second-max-320.css",
+	"/css/components/second-max-450.css",
+	"/css/components/second-max-600.css",
+	"/css/components/second-max-750.css",
+	"/css/components/second.css",
+	"/css/components/third.css",
+	"/img/anuncio-iqvia.png",
+	"/img/computador.svg",
+	"/img/favicon.png",
+	"/img/foto-rodape-raphael-freire.png",
+	"/img/icon-form-email.svg",
+	"/img/icon-form-nome.svg",
+	"/img/linhas-detalhe.svg",
+	"/img/logos.png",
+	"/img/revista-abradilan.png",
+	"/img/revista-abradimex.png",
+	"/img/revista-associadas.png",
+	"/img/revista-extrafarma.png",
+	"/js/app.js",
+	"/js/lazysizes.min.js",
+	"/js/sw.js"
+];
 
 self.addEventListener("install", function(event) {
 	// Perform install steps
