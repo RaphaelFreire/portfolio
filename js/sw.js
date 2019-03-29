58d78e32443dfee43e6800e698b6aafb04b2eb7a
Raphael Freire <raphaelmachadofreire@gmail.com>
2019-03-28 23:16:15 -0300

    adicionando favicon.svg

--- js/sw.js
+++ js/sw.js
@@ -1,7 +1,7 @@
 // sw.js
 const cacheName = "RaphaelPWA-v1";
 const filesToCache = [
-	"/index.html",
+	"index.html",
 	"/css/generic/base.css",
 	"/css/generic/reset.min.css",
 	"/css/components/footer.css",
