0a535d9baa99b04600419fbdadb28d64aa16ecca
Raphael Freire <raphaelmachadofreire@gmail.com>
2019-03-29 21:03:17 -0300

    ajustando fetch para buscar arquivo no cache

--- sw.js
+++ sw.js
@@ -73,3 +73,17 @@ self.addEventListener("fetch", event => {
 		})
 	);
 });
+
+// Serve from Cache
+this.addEventListener("fetch", event => {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(response => {
+				return response || fetch(event.request);
+			})
+			.catch(() => {
+				return caches.match("index.html");
+			})
+	);
+});
